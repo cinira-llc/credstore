c72256078f18bf1b9dc9b37929f06ee594a608fa
Matt Jensen <matt.jensen@sansio.com>
2025-08-20 07:26:21 -0500

    Compile as CommonJS module.

--- src/credstore.ts
+++ src/credstore.ts
@@ -1,20 +1,26 @@
 #!/usr/bin/env node
-import process from "process";
 import fs from "fs";
 import path from "path";
-import runWindows from "./windows.js";
+import {runWindows} from "./windows.js";
+
+const run = (home: string, args: string[]) => {
+    if (!(process.env["OS"] || "").startsWith("Windows_")) {
+        console.log(`Home: ${home}`);
+        console.log(`ARGV: ${JSON.stringify(process.argv)}`);
+    } else {
+        runWindows(home, args);
+    }
+}
 
 const cwd = process.cwd();
-const stat = await fs.promises.stat(path.resolve(cwd, "./dist"));
-let home: string;
-if (stat.isDirectory()) {
-    home = cwd;
-} else {
-    home = process.argv[1];
-}
-if (!(process.env["OS"] || "").startsWith("Windows_")) {
-    console.log(`Home: ${home}`);
-    console.log(`ARGV: ${JSON.stringify(process.argv)}`);
-} else {
-    runWindows(home, process.argv.slice(2));
-}
+fs.stat(path.resolve(cwd, "./dist"), (err: any, stats: any) => {
+    if (null != err) {
+        throw err;
+    }
+    const args = process.argv.slice(2);
+    if (stats.isDirectory()) {
+        run(cwd, args);
+    } else {
+        run(process.argv[1], args);
+    }
+});
--- src/windows.ts
+++ src/windows.ts
@@ -1,13 +1,13 @@
-import edge from "edge-js";
 import path from "path";
 
-function runWindows(home: string, args: string[]) {
+const runWindows = (home: string, args: string[]) => {
+    const edge = require("edge-js");
     const credManager = edge.func({
         assemblyFile: path.resolve(home, "./CredManagerLib/bin/Release/net48/CredManagerLib.dll"),
         typeName: "CredManager.Util",
         methodName: "Invoke"
     });
-    credManager(args, (error, result) => {
+    credManager(args, (error: any, result: any) => {
         if (error) {
             throw error;
         } else {
@@ -16,4 +16,6 @@ function runWindows(home: string, args: string[]) {
     });
 }
 
-export default runWindows;
+export {
+    runWindows
+};
