2a2a49f17e1571a1bdd4a6212bfdab82bb2a1bbb
Matt Jensen <mattj65816@gmail.com>
2025-08-24 19:25:56 -0500

    feat: implement --help and --version flags.

--- src/credstore.ts
+++ src/credstore.ts
@@ -1,30 +1,45 @@
 #!/usr/bin/env node
+import fs from "fs";
+import path from "path";
 import process from "process";
 import {Adapters} from "./adapters";
+import {findHome} from "./utils";
 
 /* Note: the Node process terminates when the event loop is empty, so this IIFE will run to completion and the exit code
 will be 0 unless we explicitly exit with some other value. */
 (async () => {
-    const adapters = await Adapters.create();
-    const adapter = await adapters.select();
-    const [, , action, service, username, password] = process.argv;
-    try {
-        switch (action) {
-            case "delete":
-                await adapter.delete(service, username);
-                break;
-            case "get":
-                console.log(await adapter.get(service, username));
-                break;
-            case "set":
-                await adapter.set(service, username, password);
-                break;
-            default:
-                console.error(`Unsupported action [${action}].`);
-                process.exit(1);
+    const {argv} = process;
+    if (-1 !== argv.indexOf("--help") || -1 !== argv.indexOf("-h") || argv.length < 3) {
+        const command = path.basename(argv[1]);
+        console.log(`Usage: ${command} delete <service> <account>`);
+        console.log(`       ${command} get <service> <account>`);
+        console.log(`       ${command} set <service> <account> <password>`);
+    } else if (-1 !== argv.indexOf("--version") || -1 !== argv.indexOf("-v")) {
+        const home = await findHome(process.cwd());
+        const version = await fs.promises.readFile(path.resolve(home, "./dist/VERSION"), "utf-8");
+        console.log(`credstore ${version}`);
+    } else {
+        const adapters = await Adapters.create();
+        const adapter = await adapters.select();
+        const [, , action, service, username, password] = argv;
+        try {
+            switch (action) {
+                case "delete":
+                    await adapter.delete(service, username);
+                    break;
+                case "get":
+                    console.log(await adapter.get(service, username));
+                    break;
+                case "set":
+                    await adapter.set(service, username, password);
+                    break;
+                default:
+                    console.error(`Unsupported action [${action}].`);
+                    process.exit(1);
+            }
+        } catch (err) {
+            console.error((err as Error).message);
+            process.exit(1);
         }
-    } catch (err) {
-        console.error((err as Error).message);
-        process.exit(1);
     }
 })();
